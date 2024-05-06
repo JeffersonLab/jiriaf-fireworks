7d2732099291fe9f9ee0b1a6032e1a377c4c3f73
tsaie79 <tsaie79@gmail.com>
2024-05-06 21:48:08 +0000

    Update main.sh script to create a log directory and mount it when launching lpad container

--- main.sh
+++ main.sh
@@ -8,7 +8,10 @@ export site=perlmutter
 export kubelet_port=10000
 export account=m4637
 
-docker run -it --rm --name=lpad \
+mkdir -p $HOME/jrm-launch/logs
+export log="$HOME/jrm-launch/logs
+
+docker run -it --rm --name=lpad -v $log:/fw/logs \
  -e nnodes=$nnodes -e nodetype=$nodetype \
   -e walltime=$walltime -e nodename=$nodename -e site=$site -e kubelet_port=$kubelet_port -e account=$account jlabtsai/jrm-fw:latest /fw/main.sh # lpad -l /fw/my_launchpad.yaml reset 
 
