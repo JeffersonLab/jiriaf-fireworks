7aa012da3ba2a1e92e6d53c5240e18e46125854a
tsaie79 <tsaie79@gmail.com>
2024-05-06 16:31:36 +0000

    Update create_db.sh script to use correct MongoDB container and credentials

--- FireWorks/util/create_db.sh
+++ FireWorks/util/create_db.sh
@@ -11,4 +11,4 @@ sleep 10
 
 # Create a new user
 echo "Creating a new user"
-docker exec -it jiriaf_test_platform_db mongosh --eval 'db.getSiblingDB("ersap_hz_scale").createUser({user: "ersap", pwd: "ersap", roles: [{role: "readWrite", db: "ersap_hz_scale"}]})'
+docker exec -it mongodb mongosh --eval 'db.getSiblingDB("jiriaf").createUser({user: "jiriaf", pwd: "jiriaf", roles: [{role: "readWrite", db: "jiriaf"}]})'
