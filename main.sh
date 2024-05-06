17f02cb809d1100a6820003d1194c2b67c759689
tsaie79 <tsaie79@gmail.com>
2024-05-06 21:52:18 +0000

    Update main.sh script to set unique kubelet_port and account number for each jrm

--- main.sh
+++ main.sh
@@ -2,11 +2,11 @@
 
 export nnodes=1
 export nodetype=cpu
-export walltime=00:20:00
+export walltime=00:20:00 # this is the walltime for the slurm job and jrm walltime
 export nodename=vk-nersc-test
 export site=perlmutter
-export kubelet_port=10000
-export account=m4637
+export kubelet_port=10000 # this should be unique for each jrm including those with the status notready. Delete the notready jrm if you want to reuse the port.
+export account=m4637 # this is the account number for the allocation at NERSC
 
 mkdir -p $HOME/jrm-launch/logs
 export log="$HOME/jrm-launch/logs"
