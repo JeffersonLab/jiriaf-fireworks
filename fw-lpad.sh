a1bc984f06694b03e8b5f0bc11f8e7a6395566e2
tsaie79 <tsaie79@gmail.com>
2024-05-06 19:23:43 +0000

    Update fw-lpad.sh to use shifter instead of docker

--- fw-lpad.sh
+++ fw-lpad.sh
@@ -2,3 +2,5 @@
 
 
 docker run -it --rm --name=lpad -v `pwd`/FireWorks:/fw  jlabtsai/fw-lpad /bin/bash -c "lpad -l /fw/my_launchpad.yaml get_wflows -t"
+
+shifter --image=docker:jlabtsai/fw-lpad /bin/bash -c "lpad -l my_launchpad.yaml get_wflows -t"
