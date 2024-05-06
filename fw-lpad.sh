bc313d786008244025d33066d79a95e18447625d
tsaie79 <tsaie79@gmail.com>
2024-05-06 16:31:44 +0000

    Update my_launchpad.yaml and fw-lpad.sh

--- fw-lpad.sh
+++ fw-lpad.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker run -it --rm --name=lpad --net=host -v `pwd`/FireWorks:/fw  jlabtsai/fw-lpad /bin/bash -c "lpad -l /fw/my_launchpad.yaml get_wflows -t"
+docker run -it --rm --name=lpad -v `pwd`/FireWorks:/fw  jlabtsai/fw-lpad /bin/bash -c "lpad -l /fw/my_launchpad.yaml get_wflows -t"
