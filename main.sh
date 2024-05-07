691e55fcd221a2c7d0596f9ae41f611cc7d586d5
tsaie79 <tsaie79@gmail.com>
2024-05-07 12:43:35 +0000

    Update Docker container name in main.sh script

--- main.sh
+++ main.sh
@@ -11,7 +11,7 @@ export account=m4637 # this is the account number for the allocation at NERSC
 mkdir -p $HOME/jrm-launch/logs
 export logs="$HOME/jrm-launch/logs"
 
-docker run -it --rm --name=lpad -v $logs:/fw/logs \
+docker run -it --rm --name=jrm-fw -v $logs:/fw/logs \
  -e nnodes=$nnodes -e nodetype=$nodetype \
   -e walltime=$walltime -e nodename=$nodename -e site=$site -e kubelet_port=$kubelet_port -e account=$account jlabtsai/jrm-fw:latest # lpad -l /fw/my_launchpad.yaml reset 
 
