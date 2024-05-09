4582d9b44fc571abc47bef07c1e89ec102d1d3ac
tsaie79 <tsaie79@gmail.com>
2024-05-09 19:45:02 +0000

    Refactor Dockerfile to use latest version of jlabtsai/jrm-fw image

--- main/main.sh
+++ main/main.sh
@@ -12,6 +12,6 @@ export logs="$HOME/jrm-launch/logs"
 
 docker run -it --rm --name=jrm-fw -v $logs:/fw/logs \
  -e nnodes=$nnodes -e nodetype=$nodetype \
-  -e walltime=$walltime -e nodename=$nodename -e site=$site -e account=$account jlabtsai/jrm-fw:test $@
+  -e walltime=$walltime -e nodename=$nodename -e site=$site -e account=$account jlabtsai/jrm-fw:latest $@
 
 
