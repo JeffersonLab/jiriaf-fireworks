7cb967b036b651309881f3210f6f09647670ccc5
tsaie79 <tsaie79@gmail.com>
2024-05-20 17:45:31 +0000

    Refactor gen_wf.py to use default values for control_plane_ip, apiserver_port, and kubeconfig if not provided in node-config.yaml

--- FireWorks/gen_wf.py
+++ FireWorks/gen_wf.py
@@ -36,10 +36,20 @@ class Jrm:
         self.node_config = loadfn(config_file) if config_file else {}
         if not self.node_config:
             raise ValueError("node-config.yaml is empty")
+        
         self.control_plane_ip = self.node_config["jrm"]["control_plane_ip"]
+        if not self.control_plane_ip:
+            self.control_plane_ip = "jiriaf2301"
+
         self.apiserver_port = self.node_config["jrm"]["apiserver_port"]
-        self.nodename = self.node_config["jrm"]["nodename"]
+        if not self.apiserver_port:
+            self.apiserver_port = "35679"
+
         self.kubeconfig = self.node_config["jrm"]["kubeconfig"]
+        if not self.kubeconfig:
+            self.kubeconfig = "/global/homes/j/jlabtsai/config/kubeconfig"
+
+        self.nodename = self.node_config["jrm"]["nodename"]
         self.vkubelet_pod_ip = self.node_config["jrm"]["vkubelet_pod_ip"]
         self.site = self.node_config["jrm"]["site"]
         self.image = self.node_config["jrm"]["image"]
