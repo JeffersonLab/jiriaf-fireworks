cebc16a4f5c20301a3ed057e9317e9631d94d95c
tsaie79 <tsaie79@gmail.com>
2024-05-21 21:00:45 +0000

    chore: Update jrm_fw_tag to use latest version

--- main/main.sh
+++ main/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export env_list="env.list"
-export jrm_fw_tag="custom-cp"
+export jrm_fw_tag="latest"
 export logs="$HOME/jrm-launch/logs"
 
 docker run -it --rm --name=jrm-fw -v $logs:/fw/logs --env-file $env_list jlabtsai/jrm-fw:$jrm_fw_tag $@w
