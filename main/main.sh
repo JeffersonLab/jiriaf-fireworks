bccd5223ddeae1ae4cab0efed18a1e35e321ba40
tsaie79 <tsaie79@gmail.com>
2024-07-22 18:46:10 +0000

    Refactor docker command in main.sh to remove unnecessary "-it" flag

--- main/main.sh
+++ main/main.sh
@@ -4,4 +4,4 @@ export env_list="env.list"
 export jrm_fw_tag="latest"
 export logs="$HOME/jrm-launch/logs"
 
-docker run -it --rm --name=jrm-fw -v $logs:/fw/logs --env-file $env_list jlabtsai/jrm-fw:$jrm_fw_tag $@w
+docker run -it --rm --name=jrm-fw -v $logs:/fw/logs --env-file $env_list jlabtsai/jrm-fw:$jrm_fw_tag $@
