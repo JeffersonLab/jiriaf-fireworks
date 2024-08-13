4c21bea4ea8cf9b6ee3b50ecb9f16440c44a5c0c
jeng-yuantsai <tsaie79@gmail.com>
2024-08-13 21:46:03 +0000

    chore: Refactor docker command in main.sh to remove unnecessary "-it" flag

--- main/main.sh
+++ main/main.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-export env_list="env.list"
-export jrm_fw_tag="latest"
+set -x
+
+# Check if site argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <site>"
+  exit 1
+fi
+
+export site=$1 # nersc / ornl
+export env_list="/home/tsai/jrm-launch/main/env.list.$site"
+export jrm_fw_tag=$site
 export logs="$HOME/jrm-launch/logs"
 
-docker run -it --rm --name=jrm-fw -v $logs:/fw/logs --env-file $env_list jlabtsai/jrm-fw:$jrm_fw_tag $@
+docker pull jlabtsai/jrm-fw:$jrm_fw_tag
+
+docker run -it --rm --name=jrm-fw -v $logs:/fw/logs --env-file $env_list jlabtsai/jrm-fw:$jrm_fw_tag "${@:2}"
