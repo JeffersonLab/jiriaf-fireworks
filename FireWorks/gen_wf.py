936271cfc697ff93bfd3850c476852fc9cddf47a
jeng-yuantsai <tsaie79@gmail.com>
2024-08-14 05:20:32 +0000

    chore: Delete duplicate code for deleting workflow from Launch Pad in gen_wf.py

--- FireWorks/gen_wf.py
+++ FireWorks/gen_wf.py
@@ -331,9 +331,9 @@ def launch_jrm_script():
     print(f"Delete nodes: {manage_ports.to_delete_knodes}")
 
     # delete wf from LaunchPad
-    for fw_id in manage_ports.to_delete_fw_ids:
+    for fw_id in set(manage_ports.to_delete_fw_ids):
         print(f"Delete workflow {fw_id} from Launch Pad")
-        # LPAD.delete_wf(int(fw_id))
+        LPAD.delete_wf(int(fw_id))
 
     time.sleep(5)
     
